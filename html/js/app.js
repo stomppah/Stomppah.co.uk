1e5b7f0b362f6e564045847bfcee25e684b08fa3
Anthony Woodward <a.woodward6237@student.leedsmet.ac.uk>
2014-06-03 18:15:52 +0100

    background

---
+++ html/js/app.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Antz on 03/06/14.
+ */
+var t = new Trianglify();
+var pattern = t.generate(document.body.clientWidth, document.body.clientHeight);
+document.body.setAttribute('style', 'background-image: '+pattern.dataUrl);
