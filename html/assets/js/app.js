f7c3c8efe79e9328aed427023e6dd74e854a14f2
Anthony Woodward <a.woodward6237@student.leedsmet.ac.uk>
2014-06-04 03:01:44 +0100

    Project is now fully integrated with Sass and Compass. This will allow an easier workflow when customising Foundation itself.

--- html/assets/js/app.js
+++ html/assets/js/app.js
@@ -1,6 +1,12 @@
 /**
  * Created by Antz on 03/06/14.
  */
+
+$(document).foundation();
+
+var doc = document.documentElement;
+doc.setAttribute('data-useragent', navigator.userAgent);
+
 var t = new Trianglify();
 var pattern = t.generate(document.body.clientWidth, document.body.clientHeight);
 document.body.setAttribute('style', 'background-image: '+pattern.dataUrl);
